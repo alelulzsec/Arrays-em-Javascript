5ab089840a686fcb3ab349aecd1a0135bc2f10b3
alexandre <alexandre-junior-2011@hotmail.com>
2018-03-27 23:42:01 -0300

    commit inicial

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var vazio = [];
+
+var numeros = ["1", "3", "4"];
+
+console.log(vazio[0]); // imprime Undefined
+
+console.log(numeros[0]); // imprime 1
+
+console.log(numeros.length); // mostra o tamanho do array numeros
+
